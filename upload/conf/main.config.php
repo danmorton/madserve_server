bbae776db587edc3ad225b5226b6f7cf1e91618e
danmorton <danmorton88@gmail.com>
2014-02-23 07:35:01 +0000

    Setup madserve..

---
+++ upload/conf/main.config.php
@@ -0,0 +1,32 @@
+
+;<?php exit; ?>
+;*** DO NOT REMOVE THE LINE ABOVE ***
+;------------------------------------------------------------------------------------------;
+; General mAdserve    Installation Settings                                                      ;
+;------------------------------------------------------------------------------------------;
+
+[madserve]
+installed                           = true
+
+;------------------------------------------------------------------------------------------;
+; Database Settings                                                                        ;
+;------------------------------------------------------------------------------------------;
+
+[database]
+type                                = mysql
+host                                = "localhost"
+socket                              =
+port                                =
+username                            = "madserve_user"
+password                            = "p@a33w0rd!"
+name                                = "madserve"
+
+[reportingdatabase]
+useseparatereportingdatabase     = false
+type                                = mysql
+host                                =
+socket                              =
+port                                =
+username                            =
+password                            =
+name                                =
