835da5880567d7f0574bf49c758e2055771dde28
Daniel Morton <dan@purplepenguin.com>
2014-03-03 01:28:29 -0500

    added whoami

--- pull_git.php
+++ pull_git.php
@@ -1,5 +1,5 @@
 <?php
 header('Content-Type: text/plain');
 echo "Attempting git pull\n";
-$r = shell_exec("cd /var/www/html && /usr/bin/git pull 2>&1");
+$r = shell_exec("whoami && cd /var/www/html && /usr/bin/git pull 2>&1");
 echo "Results:\n" . $r;
