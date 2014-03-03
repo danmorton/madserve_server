c71548f8cf591312cf62500f6fef2b37c0962f0c
Daniel Morton <dan@purplepenguin.com>
2014-03-03 00:21:54 -0500

    another

--- pull_git.php
+++ pull_git.php
@@ -3,5 +3,5 @@ header('Content-Type: text/plain');
 
 $passphrase = "f0rty6!$";
 echo "Attempting git pull\n";
-$r = shell_exec("cd /var/www/html/; git pull;");
+$r = shell_exec("cd /var/www/html && /usr/bin/git pull");
 echo "Results:\n" . $r;
